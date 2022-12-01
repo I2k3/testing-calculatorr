9fc5bc191fcf975f4584e7d2f5f47c818191a3e4
I2k3 <itzel5kari10@gmail.com>
2022-11-30 21:20:04 -0600

    Create carpetas de componentes #4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testing-calculator'`, () => {
+  it(`should have as title 'testing-calculatorr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('testing-calculator');
